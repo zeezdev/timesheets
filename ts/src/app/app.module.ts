8cb6056eb6cea1867915ce06526496b538c3ff41
Stanislav Tomilov <stomilov@iponweb.net>
2023-07-07 14:18:28 +0300

    12: work module

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatInputModule} from '@angular/material/input';
 import {MatTableModule} from '@angular/material/table';
 import {MatPaginatorModule} from '@angular/material/paginator';
@@ -12,19 +12,18 @@ import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatButtonModule} from '@angular/material/button';
 import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatDialogModule} from "@angular/material/dialog";
+import {MatDialogModule} from '@angular/material/dialog';
 // Custom
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { WorkComponent } from './work/work.component';
-import {CategoryModule} from "./category/category.module";
-import {TaskModule} from "./task/task.module";
-import {SharedModule} from "./shared/shared.module";
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {CategoryModule} from './category/category.module';
+import {TaskModule} from './task/task.module';
+import {SharedModule} from './shared/shared.module';
+import {WorkModule} from './work/work.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WorkComponent,
   ],
   imports: [
     BrowserModule,
@@ -48,6 +47,7 @@ import {SharedModule} from "./shared/shared.module";
     CategoryModule,
     TaskModule,
     SharedModule,
+    WorkModule,
   ],
   providers: [MatDialogModule],
   bootstrap: [AppComponent]
