e898d2e0d7f607d9828a27943c4cf39c923a557d
Stanislav Tomilov <stomilov@iponweb.net>
2023-07-07 21:40:58 +0300

    12: move OverworkingWatcher class into the separate unit

--- ts/src/app/shared/utils.ts
+++ ts/src/app/shared/utils.ts
@@ -1,3 +1,8 @@
-export function pad(num, size = 2) {
+export function pad(num: number, size: number = 2): string {
+  /**
+   * Convert a given number to a sring with zero-fill padding.
+   * @param num A number to converto to a string.
+   * @param size A number of zero characters in padding.
+   */
   return ('0' + num).substring(-size);
 }
